a74b0cf060d58e2710c6e712b74ea68af4e9ceb2
Edgar Cano <edge_166>
2025-06-20 11:14:13 -0600

    Test

---
+++ CC-Admin.ps1
@@ -0,0 +1,46 @@
+function Set-Admin {
+    param (
+        [string]$adminUsername,
+        [securestring]$adminPassword
+    )
+    # Cambiar contraseña del administrador y activarlo
+    Set-LocalUser -Name $adminUsername -Password $adminPassword
+    Enable-LocalUser -Name $adminUsername
+
+    Write-Host "El equipo se reiniciará en 10 segundos..."
+    Start-Sleep -Seconds 10
+    Restart-Computer -Force
+    Write-Host "Reiniciando"
+}
+
+# Verificar que se ejecute como administrador
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "Este script debe ejecutarse como administrador."
+    exit 1
+}
+
+$adminUserObj = Get-LocalUser | Where-Object { $_.SID -like '*-500' }
+if ($adminUserObj) {
+    $adminUsername = $adminUserObj
+} else {
+    Write-Error "No se pudo detectar el usuario administrador local."
+    exit 1
+}
+$adminPassword = $config.administrador.contrasegna | ConvertTo-SecureString -AsPlainText -Force
+
+Set-Admin -adminUsername $adminUsername -adminPassword $adminPassword
+
+# Quitar privilegios de administrador a todos los usuarios excepto el administrador local
+$adminGroup = [ADSI]"WinNT://./Administrators,group"
+$members = @($adminGroup.psbase.Invoke("Members")) | ForEach-Object { $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) }
+
+foreach ($member in $members) {
+  if ($member -ne $adminUsername) {
+    try {
+      $adminGroup.Remove("WinNT://./$member")
+      Write-Host "Se ha quitado $member del grupo de administradores."
+    } catch {
+      Write-Warning "No se pudo quitar $member del grupo de administradores: $_"
+    }
+  }
+}
